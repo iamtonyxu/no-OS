c36beb9698bc4d961caae029a0fcc22e6528598c
iamtonyxu <yalong.xu@foxmail.com>
2024-05-13 10:47:49 +0800

    correction: from 16-bit dac to 12-bit to avoid DC bias

--- projects/ad9361/gen_singleTone.m
+++ projects/ad9361/gen_singleTone.m
@@ -3,48 +3,42 @@ clear all;
 clc;
 
 fileName = "singleTone.txt";
-Fs = 30.72e6/2;
-Fc = 3.84e6;
-L = 1024;
+Fs = 30.72e6;
+Fc = 2.7e6;
+
+L = 8192;
+res = 16;
 t = 1/Fs*(0:L-1);
 
 %%
-signal = exp(1i*2*pi*Fc*t); 
-
-% signal1 = zeros(1, L*2);
-% 
-% for i = 1:length(signal)
-%     signal1(1, 2*i-1) = signal(1, i);
-%     signal1(1, 2*i) = signal(1, i);
-% end
-% signal = signal1;
-% 
-% Fc2 = Fs/2+1e5;
-% disturb = exp(1i*2*pi*Fc2*t);
-% 
-% figure;
-% plot(real(disturb), '.r'); hold on
-% plot(imag(disturb),'-xb');
-% 
-% signal = signal + real(disturb); 
+signal = 0.5/4*exp(1i*2*pi*Fc*t);
 
 
+%% Add dithering to avoid quantization noise
+dithering = (rand(size(signal)) - 0.5) * 2^(5-res); % 12 res bit dithering
+
+%%
+if 1
 figure;
 plot(real(signal)); hold on
 plot(imag(signal))
 
 figure;
-plot(abs(fftshift(fft(signal))), '-x');
-
+plot(abs(fftshift(fft(signal))));
+end
 %%
 if 1
 % Convert the complex waveform to fixed-point 16-bit and then to hexadecimal
-I_fixed = int16(real(signal) * 32767);
-Q_fixed = int16(imag(signal) * 32767);
+% I_fixed = int16(real(signal) * 2^(res-1)*16);
+% Q_fixed = int16(imag(signal) *  2^(res-1)*16);
+I_fixed = int16((real(signal)+dithering)*2^(res-5))*2^4;
+Q_fixed = int16((imag(signal)+dithering)*2^(res-5))*2^4;
+
+% I_fixed = int16((real(signal))*2^(res-5))*2^4;
+% Q_fixed = int16((imag(signal))*2^(res-5))*2^4;
 
 I_hex = dec2hex(typecast(I_fixed, 'uint16'), 4); % Convert to 16-bit hex
 Q_hex = dec2hex(typecast(Q_fixed, 'uint16'), 4); % Convert to 16-bit hex
-
 % Combine the hex strings into the desired format
 hexStrings = strcat('0x', I_hex, Q_hex);
 
