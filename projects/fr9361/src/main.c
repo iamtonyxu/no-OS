255770b5a3954c1073214f6ddcb360e759ee6a8e
iamtonyxu <yalong.xu@foxmail.com>
2025-09-13 10:32:18 +0800

    add 1ms delay between spi reg write 0x900 and 0x904

--- projects/fr9361/src/main.c
+++ projects/fr9361/src/main.c
@@ -170,7 +170,7 @@ int main(void)
 	//fr9361_reset
 	if(gpio_desc_resetb)
 	{
-		no_os_gpio_direction_output(gpio_desc_resetb, 0);
+		no_os_gpio_direction_output(gpio_desc_resetb, NO_OS_GPIO_OUT);
 		no_os_gpio_set_value(gpio_desc_resetb, 0); // DMM to check resetb = 0
 		no_os_mdelay(1);
 		no_os_gpio_set_value(gpio_desc_resetb, 1);// DMM to check resetb = 1
@@ -188,7 +188,9 @@ int main(void)
 	spi_hook(*spi_desc);
 
 	AD9361_WR(0x900, 0x07);
+	no_os_mdelay(1);
 	AD9361_WR(0x904, 0xA4);
+	no_os_mdelay(1);
 
 	spi_rddata = AD9361_RD(0x615);
 	spi_wrdata = 0x5a;
