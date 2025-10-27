27ddda71b3e304f627f711771101e893bb7f4adc
iamtonyxu <yalong.xu@foxmail.com>
2025-10-27 17:33:38 +0800

    copy matlab script from ad9361 to fr9361

--- projects/fr9361/analyse_cap_data.m
+++ projects/fr9361/analyse_cap_data.m
@@ -5,43 +5,54 @@ clc;
 MBit = 16;
 NFFT = 4096;
 cfg_fs_bb = 30.72e6;
-data = load('C:\tmp\cap_data.txt');
 
-% cap_data: 2t2r
-% I0(H, L), Q0(H, L)
-% I1, Q1
-% I0, Q0
-% I1, Q1
-% ...
-ch0_i = data(1:2:end, 1);
-ch0_q = data(1:2:end, 2);
-ch1_i = data(2:2:end, 1);
-ch1_q = data(2:2:end, 2);
-
-% for i = 1:length(ch0_i)
-%     ch0_i(i, 1) = swap_bytes(ch0_i(i, 1));
-%     ch0_q(i, 1) = swap_bytes(ch0_q(i, 1));
-%     ch1_i(i, 1) = swap_bytes(ch1_i(i, 1));
-%     ch1_q(i, 1) = swap_bytes(ch1_q(i, 1));
-% end
-
-for i = 1:length(ch0_i)
-    if ch0_i(i, 1) >= 2^(MBit-1)
-        ch0_i(i, 1) = -((2^MBit) - ch0_i(i, 1)); 
+%% capture data
+if 1
+    % read data from FPGA directly
+    serialport = "COM3";
+    capSize = 2048;
+    [rx0_waveform, rx1_waveform] = read_capture(serialport, capSize);
+else
+    % read data file
+    data = load('C:\tmp\cap_data.txt');
+    
+    % cap_data: 2t2r
+    % I0(H, L), Q0(H, L)
+    % I1, Q1
+    % I0, Q0
+    % I1, Q1
+    % ...
+    ch0_i = data(1:2:end, 1);
+    ch0_q = data(1:2:end, 2);
+    ch1_i = data(2:2:end, 1);
+    ch1_q = data(2:2:end, 2);
+    
+    % for i = 1:length(ch0_i)
+    %     ch0_i(i, 1) = swap_bytes(ch0_i(i, 1));
+    %     ch0_q(i, 1) = swap_bytes(ch0_q(i, 1));
+    %     ch1_i(i, 1) = swap_bytes(ch1_i(i, 1));
+    %     ch1_q(i, 1) = swap_bytes(ch1_q(i, 1));
+    % end
+    
+    for i = 1:length(ch0_i)
+        if ch0_i(i, 1) >= 2^(MBit-1)
+            ch0_i(i, 1) = -((2^MBit) - ch0_i(i, 1)); 
+        end
+        if ch0_q(i, 1) >= 2^(MBit-1)
+            ch0_q(i, 1) = -((2^MBit) - ch0_q(i, 1)); 
+        end
+        if ch1_i(i, 1) >= 2^(MBit-1)
+            ch1_i(i, 1) = -((2^MBit) - ch1_i(i, 1)); 
+        end
+        if ch1_q(i, 1) >= 2^(MBit-1)
+            ch1_q(i, 1) = -((2^MBit) - ch1_q(i, 1)); 
+        end    
     end
-    if ch0_q(i, 1) >= 2^(MBit-1)
-        ch0_q(i, 1) = -((2^MBit) - ch0_q(i, 1)); 
-    end
-    if ch1_i(i, 1) >= 2^(MBit-1)
-        ch1_i(i, 1) = -((2^MBit) - ch1_i(i, 1)); 
-    end
-    if ch1_q(i, 1) >= 2^(MBit-1)
-        ch1_q(i, 1) = -((2^MBit) - ch1_q(i, 1)); 
-    end    
+    
+    rx0_waveform = complex(ch0_i,ch0_q);
+    rx1_waveform = complex(ch1_i,ch1_q);
 end
 
-rx0_waveform = complex(ch0_q,ch0_i);
-rx1_waveform = complex(ch1_q,ch1_i);
 % figure;
 %% Plot time domain data
 subplot(2,2,1);
@@ -74,4 +85,10 @@ subplot(2,2,4);
 PlotFFT(rx1_waveform, nHarmonics, nBits, useHann, cfg_fs_bb);
 title('Rx1 Freq-domain');
 
-
+if 0
+figure;
+plot(ch0_i,'.--'); hold on
+plot(ch0_q,'.--');
+plot(ch1_i,'x--');
+plot(ch1_q,'x--');
+end
---
+++ projects/fr9361/read_capture.m
@@ -0,0 +1,82 @@
+function [rx0, rx1] = read_capture(serialCOM, capSize, waitSecond)
+HEAD = 0x5D;
+baudRate = 115200;
+
+serialCOM = upper(serialCOM);
+freeports = serialportlist("available");
+if find(freeports == serialCOM)
+    fprintf('%s is available.\n',serialCOM);
+else
+    fprintf('%s is NOT found.\n',serialCOM);
+    return;
+end
+
+if nargin < 2
+    capSize = 1024;
+    waitSecond = 5;
+elseif nargin < 3
+    waitSecond = 5;
+end
+
+capSize = capSize * 2; % rx0, rx1
+
+device = serialport(serialCOM, baudRate, "Timeout", 10);
+
+message = [uint8(HEAD), zeros(1, 9, 'uint8')];
+% Convert lengths to 3 bytes and append
+bytesCount = capSize * 2 * 2;
+bytesCount = typecast(swapbytes(uint32(bytesCount)), 'uint8'); % big-endian
+message(2:4)=bytesCount(2:4);
+
+% send read capture cmd
+write(device, message, 'uint8');
+pause(uint8(waitSecond)); % wait for response
+
+% read capture data
+%data = randi(16,1);
+data = read(device, capSize * 2 * 2, 'uint8');
+
+data_i = [];
+data_q = [];
+
+for ii = 0:capSize-1
+    data_i(end+1:end+2) = data(ii*4+(1:2));
+    data_q(end+1:end+2) = data(ii*4+(3:4));
+end
+
+signal_i = [];
+signal_q = [];
+for ii = 0:capSize-1
+    signal_i(end+1) = data_i(2*ii+2)*2^8 + data_i(2*ii+1);
+    signal_q(end+1) = data_q(2*ii+2)*2^8 + data_q(2*ii+1);
+end
+
+for i = 1:length(signal_i)
+    if signal_i(i) >= 2^15
+        signal_i(i) = signal_i(i) - 2^16;
+    end
+    if signal_q(i) >= 2^15
+        signal_q(i) = signal_q(i) - 2^16;
+    end
+end
+
+capData = signal_i + 1j*signal_q;
+rx0 = capData(1:2:end).';
+rx1 = capData(2:2:end).';
+
+%%
+if 0
+Fs = 245.76e6;
+FFT_Length = capSize;
+f = Fs*(0:(FFT_Length/2))/FFT_Length;
+
+Y = fft(capData, FFT_Length);
+P2 = abs(Y/FFT_Length);
+P1 = P2(1:FFT_Length/2+1);
+figure;
+plot(f,P1); title('signal in freq domain');
+end
+
+disp("read capture data done.");
+end
+
